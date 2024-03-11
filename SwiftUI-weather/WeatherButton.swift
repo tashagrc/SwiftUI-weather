02782ca4bc3c5e8f9267d917e3132f701fb04104
Natasha Radika <natashagradika@gmail.com>
2024-03-11 18:15:22 +0700

    init

--- SwiftUI-weather/WeatherButton.swift
+++ SwiftUI-weather/WeatherButton.swift
@@ -5,4 +5,18 @@
 //  Created by Natasha Radika on 11/03/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct WeatherButtonView: View {
+    var title: String
+    var textColor: Color
+    var backgroundColor: Color
+    var body: some View {
+        Text(title)
+            .frame(width: 200, height: 50)
+            .background(backgroundColor)
+            .foregroundColor(textColor)
+            .font(.system(size: 20, weight: .bold, design: .default))
+            .cornerRadius(10)
+    }
+}
