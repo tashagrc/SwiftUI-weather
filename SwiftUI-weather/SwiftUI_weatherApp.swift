ba422392a91e756b915644fa720363b9dd7b9e86
Natasha Radika <natashagradika@gmail.com>
2024-03-11 12:41:29 +0700

    Initial Commit

---
+++ SwiftUI-weather/SwiftUI_weatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_weatherApp.swift
+//  SwiftUI-weather
+//
+//  Created by Natasha Radika on 11/03/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_weatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
